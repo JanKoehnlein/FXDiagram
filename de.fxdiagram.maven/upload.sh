d3a9f70a29948c7f5ef244319d68feea69f8483c
Jan Koehnlein <jan.koehnlein@itemis.de>
2015-03-24 21:17:54 +0100

    Fixed maven build

--- de.fxdiagram.maven/upload.sh
+++ de.fxdiagram.maven/upload.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 pushd .
 cd bintray
+read -p "Version: " version
 read -s -p "Key: " key
 echo
 rm -f output.log
@@ -9,7 +10,7 @@ for file in `find * -type f -print`; do
 	if [ $file == 'p2/updates/content.jar' ] || [ $file == 'p2/updates/artifacts.jar' ] ; then
 		curl -# -T $file -ujankoehnlein:$key https://api.bintray.com/content/jankoehnlein/generic/$file > output.log
 	else
-		curl -# -T $file -ujankoehnlein:$key https://api.bintray.com/content/jankoehnlein/generic/FXDiagram/0.1/$file > output.log
+		curl -# -T $file -ujankoehnlein:$key https://api.bintray.com/content/jankoehnlein/generic/FXDiagram/$version/$file > output.log
 	fi
 	cat output.log
 	echo
