aeaf6d401a065cc8a33eaaea21ea0d1221e44f98
Jan Koehnlein <jan.koehnlein@itemis.de>
2015-05-08 14:18:02 +0200

    increased version to 0.4

--- de.fxdiagram.maven/update_versions.sh
+++ de.fxdiagram.maven/update_versions.sh
@@ -3,17 +3,7 @@
 read -p 'Version: ' VERSION
 
 function main() {
-	pushd .
-	cd ../
-	change_versions
-	popd
-	mvn tycho-versions:update-pom
-}
-
-function change_versions() {
-	for file in `find * -name MANIFEST.MF` ; do
-		sed -i '' -e "s/Bundle-Version: .*$/Bundle-Version: ${VERSION}.qualifier/1" $file
-	done
+	mvn tycho-versions:set-version -DnewVersion=$VERSION
 }
 
 main "$@"
