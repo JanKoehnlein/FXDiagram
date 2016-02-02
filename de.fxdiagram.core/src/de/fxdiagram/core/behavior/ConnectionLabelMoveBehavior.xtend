7bdcbe7aea2917d435a9d5b2299af60cc67c24de
Jan Koehnlein <jan.koehnlein@itemis.de>
2016-02-02 18:36:10 +0100

    [core] label move behavior for spline curves

--- de.fxdiagram.core/src/de/fxdiagram/core/behavior/ConnectionLabelMoveBehavior.xtend
+++ de.fxdiagram.core/src/de/fxdiagram/core/behavior/ConnectionLabelMoveBehavior.xtend
@@ -1,5 +1,6 @@
 package de.fxdiagram.core.behavior
 
+import de.fxdiagram.core.XConnection
 import de.fxdiagram.core.XConnectionLabel
 import de.fxdiagram.core.XControlPoint
 import de.fxdiagram.core.command.AbstractAnimationCommand
@@ -9,12 +10,17 @@ import javafx.animation.KeyFrame
 import javafx.animation.KeyValue
 import javafx.animation.Timeline
 import javafx.geometry.Point2D
+import javafx.scene.Group
 import javafx.scene.input.MouseEvent
+import javafx.scene.shape.CubicCurve
+import javafx.scene.shape.QuadCurve
+import javafx.scene.shape.Shape
 
 import static de.fxdiagram.core.XConnection.Kind.*
 import static de.fxdiagram.core.extensions.NumberExpressionExtensions.*
 import static java.lang.Math.*
 
+import static extension de.fxdiagram.core.extensions.BezierExtensions.*
 import static extension de.fxdiagram.core.extensions.Point2DExtensions.*
 
 class ConnectionLabelMoveBehavior extends MoveBehavior<XConnectionLabel> {
@@ -34,38 +40,76 @@ class ConnectionLabelMoveBehavior extends MoveBehavior<XConnectionLabel> {
 	
 	override mouseDragged(MouseEvent it) {
 		val connection = host.connection
-		if(connection.kind != POLYLINE) {
-			super.mouseDragged(it)
-		} else {
-			val controlPoints = connection.controlPoints
-			if(controlPoints.size > 1) {
-				val mouseInLocal = connection.sceneToLocal(sceneX, sceneY)
-				var currentShortestDistance = Double.MAX_VALUE
-				var currentSegmentIndex = -1
-				var currentBestK = -1.0 
-				for(i: 0..controlPoints.size-2) {
-					val segStart = controlPoints.get(i).toPoint2D
-					val segEnd = controlPoints.get(i+1).toPoint2D
-					val segDirection = segEnd - segStart
-					val segLength = segDirection.norm
-					if(segLength > EPSILON) {
-						val otherDirection = mouseInLocal - segStart
-						val lambda = (segDirection.x * otherDirection.x + segDirection.y * otherDirection.y) 
-							/ (segLength * segLength)
-						val boundedLambda = min(max(0,lambda),1)
-						val nearestOnSeg = linear(segStart, segEnd, boundedLambda)
-						val dist = (nearestOnSeg - mouseInLocal).norm
-						if(dist < currentShortestDistance) {
-							currentSegmentIndex = i
-							currentShortestDistance = dist
-							currentBestK = (boundedLambda + i)/(controlPoints.size-1)
-						}
+		val mouseInLocal = connection.sceneToLocal(sceneX, sceneY)
+		switch connection.kind { 
+			case POLYLINE:
+				mouseInLocal.dragOnPolyline(connection)
+			case QUAD_CURVE:
+				mouseInLocal.dragOnSpline(connection, QuadCurve, [$0.at($1)])
+			case CUBIC_CURVE:
+				mouseInLocal.dragOnSpline(connection, CubicCurve, [$0.at($1)])
+			default:
+				super.mouseDragged(it)
+		}
+	}
+	
+	protected def dragOnPolyline(Point2D mouseInLocal, XConnection connection) {
+		val controlPoints = connection.controlPoints
+		if(controlPoints.size > 1) {
+			var currentShortestDistance = Double.MAX_VALUE
+			var currentSegmentIndex = -1
+			var currentBestK = -1.0 
+			for(i: 0..controlPoints.size-2) {
+				val segStart = controlPoints.get(i).toPoint2D
+				val segEnd = controlPoints.get(i+1).toPoint2D
+				val segDirection = segEnd - segStart
+				val segLength = segDirection.norm
+				if(segLength > EPSILON) {
+					val otherDirection = mouseInLocal - segStart
+					val lambda = (segDirection.x * otherDirection.x + segDirection.y * otherDirection.y) 
+						/ (segLength * segLength)
+					val boundedLambda = min(max(0,lambda),1)
+					val nearestOnSeg = linear(segStart, segEnd, boundedLambda)
+					val dist = (nearestOnSeg - mouseInLocal).norm
+					if(dist < currentShortestDistance) {
+						currentSegmentIndex = i
+						currentShortestDistance = dist
+						currentBestK = (boundedLambda + i)/(controlPoints.size-1)
 					}
 				}
-				if(currentSegmentIndex != -1) {
-					host.position = currentBestK
-				}
 			}
+			if(currentSegmentIndex != -1) {
+				host.position = currentBestK
+			}
+		}
+	}
+	
+	protected def <T extends Shape> dragOnSpline(Point2D mouseInLocal, XConnection connection, Class<T> curveType, (T, double)=>Point2D at)  {
+		val curves = (connection.node as Group).children.filter(curveType)
+		var currentBestDist = Double.MAX_VALUE
+		var currentBestK = -1.0 
+		for(i: 0..<curves.size) {
+			var left = 0.0
+			var right = 1.0
+			val curve = curves.get(i)
+			var currentDistLeft = norm(at.apply(curve, left) - mouseInLocal)
+			var currentDistRight = norm(at.apply(curve, right) - mouseInLocal)
+			while (right-left > EPSILON) {
+				val mid = (left + right) / 2 
+			  	val distMid = norm(at.apply(curve, mid) - mouseInLocal)
+			  	if(currentDistRight < currentDistLeft) {
+			  		left = mid
+			  		currentDistLeft = distMid
+			  	} else {
+			  		right = mid
+			  		currentDistRight = distMid
+			  	}
+			}
+			if(currentDistLeft < currentBestDist) {
+				currentBestDist = currentDistLeft
+				currentBestK = (left + i)/(curves.size)
+			}			
+			host.position = currentBestK
 		}
 	}
 	
@@ -74,10 +118,7 @@ class ConnectionLabelMoveBehavior extends MoveBehavior<XConnectionLabel> {
 	}
 	
 	override startDrag(double screenX, double screenY) {
-		if(host.connection.kind != POLYLINE) 
-			super.startDrag(screenX, screenY)
-		else 
-			initialPosition = host.position
+		initialPosition = host.position
 	}
 	
 	override getManuallyPlaced() {
@@ -85,37 +126,32 @@ class ConnectionLabelMoveBehavior extends MoveBehavior<XConnectionLabel> {
 	}
 	
 	override protected createMoveCommand() {
-		if(host.connection.kind != POLYLINE) 
-			super.createMoveCommand
-		else
-			new AbstractAnimationCommand {
-				
-				double oldPosition
-				double newPosition
-				
-				override createExecuteAnimation(CommandContext context) {
-					oldPosition = initialPosition
-					newPosition = host.position
-					new EmptyTransition
-				}
-				
-				override createUndoAnimation(CommandContext context) {
-					new Timeline => [
-						keyFrames += new KeyFrame(context.defaultUndoDuration,
-							new KeyValue(host.positionProperty, oldPosition)
-						)
-					]
-				}
-				
-				override createRedoAnimation(CommandContext context) {
-					new Timeline => [
-						keyFrames += new KeyFrame(context.defaultUndoDuration, 
-							new KeyValue(host.positionProperty, newPosition)
-						)
-					]
-				}
-				
+		new AbstractAnimationCommand {
+			
+			double oldPosition
+			double newPosition
+			
+			override createExecuteAnimation(CommandContext context) {
+				oldPosition = initialPosition
+				newPosition = host.position
+				new EmptyTransition
 			}
+			
+			override createUndoAnimation(CommandContext context) {
+				new Timeline => [
+					keyFrames += new KeyFrame(context.defaultUndoDuration,
+						new KeyValue(host.positionProperty, oldPosition)
+					)
+				]
+			}
+			
+			override createRedoAnimation(CommandContext context) {
+				new Timeline => [
+					keyFrames += new KeyFrame(context.defaultUndoDuration, 
+						new KeyValue(host.positionProperty, newPosition)
+					)
+				]
+			}
+		}
 	}
-	
 }
--- de.fxdiagram.core/xtend-gen/de/fxdiagram/core/behavior/ConnectionLabelMoveBehavior.java
+++ de.fxdiagram.core/xtend-gen/de/fxdiagram/core/behavior/ConnectionLabelMoveBehavior.java
@@ -1,6 +1,7 @@
 package de.fxdiagram.core.behavior;
 
 import com.google.common.base.Objects;
+import com.google.common.collect.Iterables;
 import de.fxdiagram.core.XConnection;
 import de.fxdiagram.core.XConnectionLabel;
 import de.fxdiagram.core.XControlPoint;
@@ -9,6 +10,7 @@ import de.fxdiagram.core.command.AbstractAnimationCommand;
 import de.fxdiagram.core.command.AnimationCommand;
 import de.fxdiagram.core.command.CommandContext;
 import de.fxdiagram.core.command.EmptyTransition;
+import de.fxdiagram.core.extensions.BezierExtensions;
 import de.fxdiagram.core.extensions.NumberExpressionExtensions;
 import de.fxdiagram.core.extensions.Point2DExtensions;
 import javafx.animation.Animation;
@@ -18,9 +20,18 @@ import javafx.animation.Timeline;
 import javafx.beans.property.DoubleProperty;
 import javafx.collections.ObservableList;
 import javafx.geometry.Point2D;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.CubicCurve;
+import javafx.scene.shape.QuadCurve;
+import javafx.scene.shape.Shape;
 import javafx.util.Duration;
+import org.eclipse.xtext.xbase.lib.Conversions;
+import org.eclipse.xtext.xbase.lib.ExclusiveRange;
+import org.eclipse.xtext.xbase.lib.Functions.Function2;
 import org.eclipse.xtext.xbase.lib.IntegerRange;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
 import org.eclipse.xtext.xbase.lib.ObjectExtensions;
 import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
 
@@ -53,62 +64,130 @@ public class ConnectionLabelMoveBehavior extends MoveBehavior<XConnectionLabel> 
   public void mouseDragged(final MouseEvent it) {
     XConnectionLabel _host = this.getHost();
     final XConnection connection = _host.getConnection();
+    double _sceneX = it.getSceneX();
+    double _sceneY = it.getSceneY();
+    final Point2D mouseInLocal = connection.sceneToLocal(_sceneX, _sceneY);
     XConnection.Kind _kind = connection.getKind();
-    boolean _notEquals = (!Objects.equal(_kind, XConnection.Kind.POLYLINE));
-    if (_notEquals) {
-      super.mouseDragged(it);
+    if (_kind != null) {
+      switch (_kind) {
+        case POLYLINE:
+          this.dragOnPolyline(mouseInLocal, connection);
+          break;
+        case QUAD_CURVE:
+          final Function2<QuadCurve, Double, Point2D> _function = (QuadCurve $0, Double $1) -> {
+            return BezierExtensions.at($0, ($1).doubleValue());
+          };
+          this.<QuadCurve>dragOnSpline(mouseInLocal, connection, QuadCurve.class, _function);
+          break;
+        case CUBIC_CURVE:
+          final Function2<CubicCurve, Double, Point2D> _function_1 = (CubicCurve $0, Double $1) -> {
+            return BezierExtensions.at($0, ($1).doubleValue());
+          };
+          this.<CubicCurve>dragOnSpline(mouseInLocal, connection, CubicCurve.class, _function_1);
+          break;
+        default:
+          super.mouseDragged(it);
+          break;
+      }
     } else {
-      final ObservableList<XControlPoint> controlPoints = connection.getControlPoints();
-      int _size = controlPoints.size();
-      boolean _greaterThan = (_size > 1);
-      if (_greaterThan) {
-        double _sceneX = it.getSceneX();
-        double _sceneY = it.getSceneY();
-        final Point2D mouseInLocal = connection.sceneToLocal(_sceneX, _sceneY);
-        double currentShortestDistance = Double.MAX_VALUE;
-        int currentSegmentIndex = (-1);
-        double currentBestK = (-1.0);
-        int _size_1 = controlPoints.size();
-        int _minus = (_size_1 - 2);
-        IntegerRange _upTo = new IntegerRange(0, _minus);
-        for (final Integer i : _upTo) {
-          {
-            XControlPoint _get = controlPoints.get((i).intValue());
-            final Point2D segStart = this.toPoint2D(_get);
-            XControlPoint _get_1 = controlPoints.get(((i).intValue() + 1));
-            final Point2D segEnd = this.toPoint2D(_get_1);
-            final Point2D segDirection = Point2DExtensions.operator_minus(segEnd, segStart);
-            final double segLength = Point2DExtensions.norm(segDirection);
-            if ((segLength > NumberExpressionExtensions.EPSILON)) {
-              final Point2D otherDirection = Point2DExtensions.operator_minus(mouseInLocal, segStart);
-              double _x = segDirection.getX();
-              double _x_1 = otherDirection.getX();
-              double _multiply = (_x * _x_1);
-              double _y = segDirection.getY();
-              double _y_1 = otherDirection.getY();
-              double _multiply_1 = (_y * _y_1);
-              double _plus = (_multiply + _multiply_1);
-              final double lambda = (_plus / (segLength * segLength));
-              double _max = Math.max(0, lambda);
-              final double boundedLambda = Math.min(_max, 1);
-              final Point2D nearestOnSeg = Point2DExtensions.linear(segStart, segEnd, boundedLambda);
-              Point2D _minus_1 = Point2DExtensions.operator_minus(nearestOnSeg, mouseInLocal);
-              final double dist = Point2DExtensions.norm(_minus_1);
-              if ((dist < currentShortestDistance)) {
-                currentSegmentIndex = (i).intValue();
-                currentShortestDistance = dist;
-                int _size_2 = controlPoints.size();
-                int _minus_2 = (_size_2 - 1);
-                double _divide = ((boundedLambda + (i).intValue()) / _minus_2);
-                currentBestK = _divide;
-              }
+      super.mouseDragged(it);
+    }
+  }
+  
+  protected void dragOnPolyline(final Point2D mouseInLocal, final XConnection connection) {
+    final ObservableList<XControlPoint> controlPoints = connection.getControlPoints();
+    int _size = controlPoints.size();
+    boolean _greaterThan = (_size > 1);
+    if (_greaterThan) {
+      double currentShortestDistance = Double.MAX_VALUE;
+      int currentSegmentIndex = (-1);
+      double currentBestK = (-1.0);
+      int _size_1 = controlPoints.size();
+      int _minus = (_size_1 - 2);
+      IntegerRange _upTo = new IntegerRange(0, _minus);
+      for (final Integer i : _upTo) {
+        {
+          XControlPoint _get = controlPoints.get((i).intValue());
+          final Point2D segStart = this.toPoint2D(_get);
+          XControlPoint _get_1 = controlPoints.get(((i).intValue() + 1));
+          final Point2D segEnd = this.toPoint2D(_get_1);
+          final Point2D segDirection = Point2DExtensions.operator_minus(segEnd, segStart);
+          final double segLength = Point2DExtensions.norm(segDirection);
+          if ((segLength > NumberExpressionExtensions.EPSILON)) {
+            final Point2D otherDirection = Point2DExtensions.operator_minus(mouseInLocal, segStart);
+            double _x = segDirection.getX();
+            double _x_1 = otherDirection.getX();
+            double _multiply = (_x * _x_1);
+            double _y = segDirection.getY();
+            double _y_1 = otherDirection.getY();
+            double _multiply_1 = (_y * _y_1);
+            double _plus = (_multiply + _multiply_1);
+            final double lambda = (_plus / (segLength * segLength));
+            double _max = Math.max(0, lambda);
+            final double boundedLambda = Math.min(_max, 1);
+            final Point2D nearestOnSeg = Point2DExtensions.linear(segStart, segEnd, boundedLambda);
+            Point2D _minus_1 = Point2DExtensions.operator_minus(nearestOnSeg, mouseInLocal);
+            final double dist = Point2DExtensions.norm(_minus_1);
+            if ((dist < currentShortestDistance)) {
+              currentSegmentIndex = (i).intValue();
+              currentShortestDistance = dist;
+              int _size_2 = controlPoints.size();
+              int _minus_2 = (_size_2 - 1);
+              double _divide = ((boundedLambda + (i).intValue()) / _minus_2);
+              currentBestK = _divide;
             }
           }
         }
-        if ((currentSegmentIndex != (-1))) {
-          XConnectionLabel _host_1 = this.getHost();
-          _host_1.setPosition(currentBestK);
+      }
+      if ((currentSegmentIndex != (-1))) {
+        XConnectionLabel _host = this.getHost();
+        _host.setPosition(currentBestK);
+      }
+    }
+  }
+  
+  protected <T extends Shape> void dragOnSpline(final Point2D mouseInLocal, final XConnection connection, final Class<T> curveType, final Function2<? super T, ? super Double, ? extends Point2D> at) {
+    Node _node = connection.getNode();
+    ObservableList<Node> _children = ((Group) _node).getChildren();
+    final Iterable<T> curves = Iterables.<T>filter(_children, curveType);
+    double currentBestDist = Double.MAX_VALUE;
+    double currentBestK = (-1.0);
+    int _size = IterableExtensions.size(curves);
+    ExclusiveRange _doubleDotLessThan = new ExclusiveRange(0, _size, true);
+    for (final Integer i : _doubleDotLessThan) {
+      {
+        double left = 0.0;
+        double right = 1.0;
+        final T curve = ((T[])Conversions.unwrapArray(curves, Shape.class))[(i).intValue()];
+        Point2D _apply = at.apply(curve, Double.valueOf(left));
+        Point2D _minus = Point2DExtensions.operator_minus(_apply, mouseInLocal);
+        double currentDistLeft = Point2DExtensions.norm(_minus);
+        Point2D _apply_1 = at.apply(curve, Double.valueOf(right));
+        Point2D _minus_1 = Point2DExtensions.operator_minus(_apply_1, mouseInLocal);
+        double currentDistRight = Point2DExtensions.norm(_minus_1);
+        while (((right - left) > NumberExpressionExtensions.EPSILON)) {
+          {
+            final double mid = ((left + right) / 2);
+            Point2D _apply_2 = at.apply(curve, Double.valueOf(mid));
+            Point2D _minus_2 = Point2DExtensions.operator_minus(_apply_2, mouseInLocal);
+            final double distMid = Point2DExtensions.norm(_minus_2);
+            if ((currentDistRight < currentDistLeft)) {
+              left = mid;
+              currentDistLeft = distMid;
+            } else {
+              right = mid;
+              currentDistRight = distMid;
+            }
+          }
         }
+        if ((currentDistLeft < currentBestDist)) {
+          currentBestDist = currentDistLeft;
+          int _size_1 = IterableExtensions.size(curves);
+          double _divide = ((left + (i).intValue()) / _size_1);
+          currentBestK = _divide;
+        }
+        XConnectionLabel _host = this.getHost();
+        _host.setPosition(currentBestK);
       }
     }
   }
@@ -122,16 +201,8 @@ public class ConnectionLabelMoveBehavior extends MoveBehavior<XConnectionLabel> 
   @Override
   public void startDrag(final double screenX, final double screenY) {
     XConnectionLabel _host = this.getHost();
-    XConnection _connection = _host.getConnection();
-    XConnection.Kind _kind = _connection.getKind();
-    boolean _notEquals = (!Objects.equal(_kind, XConnection.Kind.POLYLINE));
-    if (_notEquals) {
-      super.startDrag(screenX, screenY);
-    } else {
-      XConnectionLabel _host_1 = this.getHost();
-      double _position = _host_1.getPosition();
-      this.initialPosition = _position;
-    }
+    double _position = _host.getPosition();
+    this.initialPosition = _position;
   }
   
   @Override
@@ -149,59 +220,49 @@ public class ConnectionLabelMoveBehavior extends MoveBehavior<XConnectionLabel> 
       double newPosition;
     }
     
-    AnimationCommand _xifexpression = null;
-    XConnectionLabel _host = this.getHost();
-    XConnection _connection = _host.getConnection();
-    XConnection.Kind _kind = _connection.getKind();
-    boolean _notEquals = (!Objects.equal(_kind, XConnection.Kind.POLYLINE));
-    if (_notEquals) {
-      _xifexpression = super.createMoveCommand();
-    } else {
-      _xifexpression = new __ConnectionLabelMoveBehavior_1() {
-        @Override
-        public Animation createExecuteAnimation(final CommandContext context) {
-          EmptyTransition _xblockexpression = null;
-          {
-            this.oldPosition = ConnectionLabelMoveBehavior.this.initialPosition;
-            XConnectionLabel _host = ConnectionLabelMoveBehavior.this.getHost();
-            double _position = _host.getPosition();
-            this.newPosition = _position;
-            _xblockexpression = new EmptyTransition();
-          }
-          return _xblockexpression;
+    return new __ConnectionLabelMoveBehavior_1() {
+      @Override
+      public Animation createExecuteAnimation(final CommandContext context) {
+        EmptyTransition _xblockexpression = null;
+        {
+          this.oldPosition = ConnectionLabelMoveBehavior.this.initialPosition;
+          XConnectionLabel _host = ConnectionLabelMoveBehavior.this.getHost();
+          double _position = _host.getPosition();
+          this.newPosition = _position;
+          _xblockexpression = new EmptyTransition();
         }
-        
-        @Override
-        public Animation createUndoAnimation(final CommandContext context) {
-          Timeline _timeline = new Timeline();
-          final Procedure1<Timeline> _function = (Timeline it) -> {
-            ObservableList<KeyFrame> _keyFrames = it.getKeyFrames();
-            Duration _defaultUndoDuration = context.getDefaultUndoDuration();
-            XConnectionLabel _host = ConnectionLabelMoveBehavior.this.getHost();
-            DoubleProperty _positionProperty = _host.positionProperty();
-            KeyValue _keyValue = new <Number>KeyValue(_positionProperty, Double.valueOf(this.oldPosition));
-            KeyFrame _keyFrame = new KeyFrame(_defaultUndoDuration, _keyValue);
-            _keyFrames.add(_keyFrame);
-          };
-          return ObjectExtensions.<Timeline>operator_doubleArrow(_timeline, _function);
-        }
-        
-        @Override
-        public Animation createRedoAnimation(final CommandContext context) {
-          Timeline _timeline = new Timeline();
-          final Procedure1<Timeline> _function = (Timeline it) -> {
-            ObservableList<KeyFrame> _keyFrames = it.getKeyFrames();
-            Duration _defaultUndoDuration = context.getDefaultUndoDuration();
-            XConnectionLabel _host = ConnectionLabelMoveBehavior.this.getHost();
-            DoubleProperty _positionProperty = _host.positionProperty();
-            KeyValue _keyValue = new <Number>KeyValue(_positionProperty, Double.valueOf(this.newPosition));
-            KeyFrame _keyFrame = new KeyFrame(_defaultUndoDuration, _keyValue);
-            _keyFrames.add(_keyFrame);
-          };
-          return ObjectExtensions.<Timeline>operator_doubleArrow(_timeline, _function);
-        }
-      };
-    }
-    return _xifexpression;
+        return _xblockexpression;
+      }
+      
+      @Override
+      public Animation createUndoAnimation(final CommandContext context) {
+        Timeline _timeline = new Timeline();
+        final Procedure1<Timeline> _function = (Timeline it) -> {
+          ObservableList<KeyFrame> _keyFrames = it.getKeyFrames();
+          Duration _defaultUndoDuration = context.getDefaultUndoDuration();
+          XConnectionLabel _host = ConnectionLabelMoveBehavior.this.getHost();
+          DoubleProperty _positionProperty = _host.positionProperty();
+          KeyValue _keyValue = new <Number>KeyValue(_positionProperty, Double.valueOf(this.oldPosition));
+          KeyFrame _keyFrame = new KeyFrame(_defaultUndoDuration, _keyValue);
+          _keyFrames.add(_keyFrame);
+        };
+        return ObjectExtensions.<Timeline>operator_doubleArrow(_timeline, _function);
+      }
+      
+      @Override
+      public Animation createRedoAnimation(final CommandContext context) {
+        Timeline _timeline = new Timeline();
+        final Procedure1<Timeline> _function = (Timeline it) -> {
+          ObservableList<KeyFrame> _keyFrames = it.getKeyFrames();
+          Duration _defaultUndoDuration = context.getDefaultUndoDuration();
+          XConnectionLabel _host = ConnectionLabelMoveBehavior.this.getHost();
+          DoubleProperty _positionProperty = _host.positionProperty();
+          KeyValue _keyValue = new <Number>KeyValue(_positionProperty, Double.valueOf(this.newPosition));
+          KeyFrame _keyFrame = new KeyFrame(_defaultUndoDuration, _keyValue);
+          _keyFrames.add(_keyFrame);
+        };
+        return ObjectExtensions.<Timeline>operator_doubleArrow(_timeline, _function);
+      }
+    };
   }
 }
