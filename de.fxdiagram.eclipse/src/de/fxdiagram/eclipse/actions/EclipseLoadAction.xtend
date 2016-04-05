99b67a566a24c25aff24589453e5c46f2ee89ffc
Jan Koehnlein <jan.koehnlein@typefox.io>
2016-04-05 21:17:42 +0200

    #49  [eclipse] Load diagram dialog is not an eclipse dialog

--- de.fxdiagram.eclipse/src/de/fxdiagram/eclipse/actions/EclipseLoadAction.xtend
+++ de.fxdiagram.eclipse/src/de/fxdiagram/eclipse/actions/EclipseLoadAction.xtend
@@ -4,31 +4,49 @@ import de.fxdiagram.core.XRoot
 import de.fxdiagram.core.model.ModelLoad
 import de.fxdiagram.core.tools.actions.LoadAction
 import java.io.InputStreamReader
-import javafx.stage.FileChooser
+import org.eclipse.core.resources.IFile
 import org.eclipse.core.resources.IResource
 import org.eclipse.core.resources.ResourcesPlugin
 import org.eclipse.core.runtime.NullProgressMonitor
-
-import static extension de.fxdiagram.eclipse.actions.FileExtensions.*
+import org.eclipse.jface.window.Window
+import org.eclipse.swt.widgets.Display
+import org.eclipse.ui.dialogs.ResourceListSelectionDialog
+import org.eclipse.swt.widgets.Composite
+import org.eclipse.swt.widgets.Text
 
 class EclipseLoadAction extends LoadAction {
 	
 	override perform(XRoot root) {
-		val workspaceDir = ResourcesPlugin.workspace.root.location.toFile
-		val fileChooser = new FileChooser() => [
-			title = 'Load diagram'
-			extensionFilters += new FileChooser.ExtensionFilter("FXDiagram", "*.fxd")
-			initialDirectory = workspaceDir
-		]
-		val file = fileChooser.showOpenDialog(root.scene.window).toWorkspaceFile
-		file.refreshLocal(IResource.DEPTH_ONE, new NullProgressMonitor)
-		if(file?.exists) {
-			val node = new ModelLoad().load(new InputStreamReader(file.contents, file.charset))
-			if(node instanceof XRoot) {
-				root.replaceDomainObjectProviders(node.domainObjectProviders)
-				root.rootDiagram = node.diagram
-				root.fileName = file.fullPath.toOSString
+        val shell = Display.^default.activeShell
+		val workspaceRoot = ResourcesPlugin.workspace.root
+		val dialog = new ResourceListSelectionDialog(shell, workspaceRoot, IResource.FILE) {
+			override protected createDialogArea(Composite parent) {
+				val control = super.createDialogArea(parent)
+				parent.findTextWidget?.setText('*.fxd')
+				control 
 			}
 		}
-	}
+        if (dialog.open == Window.OK && dialog.result?.length == 1) {
+            val file = dialog.result.head as IFile
+            file.refreshLocal(IResource.DEPTH_ONE, new NullProgressMonitor)
+            if (file?.exists) {
+                val node = new ModelLoad().load(new InputStreamReader(file.contents, file.charset))
+                if (node instanceof XRoot) {
+                    root.replaceDomainObjectProviders(node.domainObjectProviders)
+                    root.rootDiagram = node.diagram
+                    root.fileName = file.fullPath.toOSString
+                }
+            }
+        }
+    }
+    
+    private def Text findTextWidget(Composite composite) {
+    	composite.children.map[
+    		switch it {
+    			Text: it
+    			Composite: findTextWidget
+    			default: null
+    		}
+   		].filterNull.head
+    }
 }
--- de.fxdiagram.eclipse/xtend-gen/de/fxdiagram/eclipse/actions/EclipseLoadAction.java
+++ de.fxdiagram.eclipse/xtend-gen/de/fxdiagram/eclipse/actions/EclipseLoadAction.java
@@ -5,14 +5,10 @@ import de.fxdiagram.core.XRoot;
 import de.fxdiagram.core.model.DomainObjectProvider;
 import de.fxdiagram.core.model.ModelLoad;
 import de.fxdiagram.core.tools.actions.LoadAction;
-import de.fxdiagram.eclipse.actions.FileExtensions;
-import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.List;
 import javafx.collections.ObservableList;
-import javafx.scene.Scene;
-import javafx.stage.FileChooser;
-import javafx.stage.Window;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspace;
@@ -20,56 +16,113 @@ import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.ui.dialogs.ResourceListSelectionDialog;
+import org.eclipse.xtext.xbase.lib.Conversions;
 import org.eclipse.xtext.xbase.lib.Exceptions;
-import org.eclipse.xtext.xbase.lib.ObjectExtensions;
-import org.eclipse.xtext.xbase.lib.Procedures.Procedure1;
+import org.eclipse.xtext.xbase.lib.Functions.Function1;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.eclipse.xtext.xbase.lib.ListExtensions;
 
 @SuppressWarnings("all")
 public class EclipseLoadAction extends LoadAction {
   @Override
   public void perform(final XRoot root) {
     try {
+      Display _default = Display.getDefault();
+      final Shell shell = _default.getActiveShell();
       IWorkspace _workspace = ResourcesPlugin.getWorkspace();
-      IWorkspaceRoot _root = _workspace.getRoot();
-      IPath _location = _root.getLocation();
-      final File workspaceDir = _location.toFile();
-      FileChooser _fileChooser = new FileChooser();
-      final Procedure1<FileChooser> _function = (FileChooser it) -> {
-        it.setTitle("Load diagram");
-        ObservableList<FileChooser.ExtensionFilter> _extensionFilters = it.getExtensionFilters();
-        FileChooser.ExtensionFilter _extensionFilter = new FileChooser.ExtensionFilter("FXDiagram", "*.fxd");
-        _extensionFilters.add(_extensionFilter);
-        it.setInitialDirectory(workspaceDir);
+      final IWorkspaceRoot workspaceRoot = _workspace.getRoot();
+      final ResourceListSelectionDialog dialog = new ResourceListSelectionDialog(shell, workspaceRoot, IResource.FILE) {
+        @Override
+        protected Control createDialogArea(final Composite parent) {
+          Control _xblockexpression = null;
+          {
+            final Control control = super.createDialogArea(parent);
+            Text _findTextWidget = EclipseLoadAction.this.findTextWidget(parent);
+            if (_findTextWidget!=null) {
+              _findTextWidget.setText("*.fxd");
+            }
+            _xblockexpression = control;
+          }
+          return _xblockexpression;
+        }
       };
-      final FileChooser fileChooser = ObjectExtensions.<FileChooser>operator_doubleArrow(_fileChooser, _function);
-      Scene _scene = root.getScene();
-      Window _window = _scene.getWindow();
-      File _showOpenDialog = fileChooser.showOpenDialog(_window);
-      final IFile file = FileExtensions.toWorkspaceFile(_showOpenDialog);
-      NullProgressMonitor _nullProgressMonitor = new NullProgressMonitor();
-      file.refreshLocal(IResource.DEPTH_ONE, _nullProgressMonitor);
-      boolean _exists = false;
-      if (file!=null) {
-        _exists=file.exists();
+      boolean _and = false;
+      int _open = dialog.open();
+      boolean _equals = (_open == Window.OK);
+      if (!_equals) {
+        _and = false;
+      } else {
+        Object[] _result = dialog.getResult();
+        int _length = 0;
+        if (_result!=null) {
+          _length=_result.length;
+        }
+        boolean _equals_1 = (_length == 1);
+        _and = _equals_1;
       }
-      if (_exists) {
-        ModelLoad _modelLoad = new ModelLoad();
-        InputStream _contents = file.getContents();
-        String _charset = file.getCharset();
-        InputStreamReader _inputStreamReader = new InputStreamReader(_contents, _charset);
-        final Object node = _modelLoad.load(_inputStreamReader);
-        if ((node instanceof XRoot)) {
-          ObservableList<DomainObjectProvider> _domainObjectProviders = ((XRoot)node).getDomainObjectProviders();
-          root.replaceDomainObjectProviders(_domainObjectProviders);
-          XDiagram _diagram = ((XRoot)node).getDiagram();
-          root.setRootDiagram(_diagram);
-          IPath _fullPath = file.getFullPath();
-          String _oSString = _fullPath.toOSString();
-          root.setFileName(_oSString);
+      if (_and) {
+        Object[] _result_1 = dialog.getResult();
+        Object _head = IterableExtensions.<Object>head(((Iterable<Object>)Conversions.doWrapArray(_result_1)));
+        final IFile file = ((IFile) _head);
+        NullProgressMonitor _nullProgressMonitor = new NullProgressMonitor();
+        file.refreshLocal(IResource.DEPTH_ONE, _nullProgressMonitor);
+        boolean _exists = false;
+        if (file!=null) {
+          _exists=file.exists();
+        }
+        if (_exists) {
+          ModelLoad _modelLoad = new ModelLoad();
+          InputStream _contents = file.getContents();
+          String _charset = file.getCharset();
+          InputStreamReader _inputStreamReader = new InputStreamReader(_contents, _charset);
+          final Object node = _modelLoad.load(_inputStreamReader);
+          if ((node instanceof XRoot)) {
+            ObservableList<DomainObjectProvider> _domainObjectProviders = ((XRoot)node).getDomainObjectProviders();
+            root.replaceDomainObjectProviders(_domainObjectProviders);
+            XDiagram _diagram = ((XRoot)node).getDiagram();
+            root.setRootDiagram(_diagram);
+            IPath _fullPath = file.getFullPath();
+            String _oSString = _fullPath.toOSString();
+            root.setFileName(_oSString);
+          }
         }
       }
     } catch (Throwable _e) {
       throw Exceptions.sneakyThrow(_e);
     }
+  }
+  
+  private Text findTextWidget(final Composite composite) {
+    Control[] _children = composite.getChildren();
+    final Function1<Control, Text> _function = (Control it) -> {
+      Text _switchResult = null;
+      boolean _matched = false;
+      if (!_matched) {
+        if (it instanceof Text) {
+          _matched=true;
+          _switchResult = ((Text)it);
+        }
+      }
+      if (!_matched) {
+        if (it instanceof Composite) {
+          _matched=true;
+          _switchResult = this.findTextWidget(((Composite)it));
+        }
+      }
+      if (!_matched) {
+        _switchResult = null;
+      }
+      return _switchResult;
+    };
+    List<Text> _map = ListExtensions.<Control, Text>map(((List<Control>)Conversions.doWrapArray(_children)), _function);
+    Iterable<Text> _filterNull = IterableExtensions.<Text>filterNull(_map);
+    return IterableExtensions.<Text>head(_filterNull);
   }
 }
